ddab7d87dc1929d59f54bfd48c51bf638fc470f3
Antoine Alberti <aalberti@prima-solutions.com>
2018-06-15 11:54:36 +0200

    remove useless object Bowling

--- src/main/kotlin/aa/Bowling.kt
+++ src/main/kotlin/aa/Bowling.kt
@@ -1,14 +1,12 @@
 package aa
 
-object Bowling {
-    fun score(vararg tries: String): Int = tries.asIterable()
-            .windowed(size = 3, step = 2, partialWindows = true)
-            .map {
-                when {
-                    it.size == 1 -> it[0].toInt()
-                    it[1] == "/" -> 10 + it[2].toInt()
-                    else -> it[0].toInt() + it[1].toInt()
-                }
+fun score(vararg tries: String): Int = tries.asIterable()
+        .windowed(size = 3, step = 2, partialWindows = true)
+        .map {
+            when {
+                it.size == 1 -> it[0].toInt()
+                it[1] == "/" -> 10 + it[2].toInt()
+                else -> it[0].toInt() + it[1].toInt()
             }
-            .sum()
-}
+        }
+        .sum()
--- src/test/kotlin/aa/BowlingTest.kt
+++ src/test/kotlin/aa/BowlingTest.kt
@@ -5,15 +5,15 @@ import io.kotlintest.specs.StringSpec
 
 class BowlingTest : StringSpec({
     "start at 0" {
-        Bowling.score() shouldBe 0
+        score() shouldBe 0
     }
 
     "count 1st try" {
-        Bowling.score("1") shouldBe 1
+        score("1") shouldBe 1
     }
 
     "count all tries" {
-        Bowling.score(
+        score(
                 "1", "1",
                 "1", "1",
                 "1", "1",
@@ -24,24 +24,27 @@ class BowlingTest : StringSpec({
                 "1", "1",
                 "1", "1",
                 "1", "1"
-                ) shouldBe 20
+        ) shouldBe 20
     }
 
     "spare is 10" {
-        Bowling.score(
+        score(
                 "0", "/",
-                "0") shouldBe 10
+                "0"
+        ) shouldBe 10
     }
 
-    "spare is 10 for the frame" {
-        Bowling.score(
+    "spare is 10 for the whole frame" {
+        score(
                 "2", "/",
-                "0") shouldBe 10
+                "0"
+        ) shouldBe 10
     }
 
     "spare is 10 + next try" {
-        Bowling.score(
+        score(
                 "0", "/",
-                "1") shouldBe 12
+                "1"
+        ) shouldBe 12
     }
 })
