3ef79a26db059bcdb9818395461f95f08d1a9fd1
Antoine Alberti <aa@antoinealberti.com>
2018-06-22 17:37:41 +0200

    make toFrames tail recursive by turning toFrames upside down and get rid of toFrames' null return value

--- src/main/kotlin/aa/Bowling.kt
+++ src/main/kotlin/aa/Bowling.kt
@@ -6,43 +6,47 @@ fun score(vararg tries: String): Int {
             ?.score() ?: 0
 }
 
-fun List<String>.toFrames(): Frames? {
-    return if (isEmpty())
-        null
-    else {
-        val frameSize = if (this[0] == "X") 1 else 2
-        val next = drop(frameSize).toFrames()
-        when {
-            this[0] == "X" -> Strike(
-                    next
+fun List<String>.toFrames(): Frame? = toFrames(null)
+
+private fun List<String>.toFrames(nextFrame: Frame?): Frame? = when {
+    isEmpty() -> nextFrame
+    last() == "X" -> dropLast(1).toFrames(
+            Strike(
+                    nextFrame
             )
-            this[1] == "/" -> Spare(
-                    this[0].value(),
-                    next
+    )
+    last() == "/" -> dropLast(2).toFrames(
+            Spare(
+                    dropLast(1).last().value(),
+                    nextFrame
             )
-            else -> Incomplete(
-                    this[0].value(),
-                    this[1].value(),
-                    next
+    )
+    else -> dropLast(2).toFrames(
+            Incomplete(
+                    dropLast(1).last().value(),
+                    last().value(),
+                    nextFrame
             )
-        }
-    }
+    )
 }
 
-sealed class Frames(open val first: Int, open val next: Frames?) {
-    fun score():Int = frameScore() + (next?.score() ?: 0)
+sealed class Frame(open val first: Int, open val next: Frame?) {
+    fun score(): Int = frameScore() + (next?.score() ?: 0)
     abstract fun frameScore(): Int
     abstract val firstTwo: Int?
 }
-data class Strike(override val next: Frames?) : Frames(10, next) {
+
+data class Strike(override val next: Frame?) : Frame(10, next) {
     override fun frameScore(): Int = if (next?.firstTwo == null) 0 else 10 + (next.firstTwo ?: 0)
     override val firstTwo: Int? = if (next == null) null else 10 + next.first
 }
-data class Spare(override val first: Int, override val next: Frames?) : Frames(first, next) {
+
+data class Spare(override val first: Int, override val next: Frame?) : Frame(first, next) {
     override fun frameScore(): Int = if (next == null) 0 else 10 + next.first
     override val firstTwo: Int = 10
 }
-data class Incomplete(override val first: Int, val second: Int, override val next: Frames?) : Frames(first, next) {
+
+data class Incomplete(override val first: Int, val second: Int, override val next: Frame?) : Frame(first, next) {
     override fun frameScore(): Int = first + second
     override val firstTwo: Int = first + second
 }
