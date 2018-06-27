b2eaa1889a9cba25ff440b5a8b19aa15e07d0b4d
Antoine Alberti <aa@antoinealberti.com>
2018-06-27 16:14:43 +0200

    extract toFrames

--- src/main/kotlin/aa/Bowling.kt
+++ src/main/kotlin/aa/Bowling.kt
@@ -1,23 +1,26 @@
 package aa
 
 fun score(vararg tries: String): Int = tries.toList()
+        .toFrames()
+        .map { it.score() }
+        .sum()
+
+private fun List<String>.toFrames(): List<Frame> = this
         .windowed(size = 3, step = 2, partialWindows = true)
         .map { it.toFrame() }
-        .map { it.value() }
-        .sum()
 
 private fun List<String>.toFrame(): Frame = if (this[1] == "/") Spare(this[0], this[2]) else Incomplete(this[0], this[1])
 
 sealed class Frame {
-    abstract fun value(): Int
+    abstract fun score(): Int
 }
 
 data class Spare(val first: String, val next: String?) : Frame() {
-    override fun value(): Int = 10 + (next?.toInt() ?: 0)
+    override fun score(): Int = 10 + (next?.toInt() ?: 0)
 }
 
-data class Incomplete(val first: String, val second: String): Frame() {
-    override fun value(): Int {
+data class Incomplete(val first: String, val second: String) : Frame() {
+    override fun score(): Int {
         return first.toInt() + second.toInt()
     }
 }
