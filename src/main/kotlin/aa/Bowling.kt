045d6b6ee97d1f429eab6a110736761b6cbaa508
Antoine Alberti <aa@antoinealberti.com>
2018-06-27 15:00:27 +0200

    introduce Frame

--- src/main/kotlin/aa/Bowling.kt
+++ src/main/kotlin/aa/Bowling.kt
@@ -2,10 +2,15 @@ package aa
 
 fun score(vararg tries: String): Int = tries.toList()
         .windowed(size = 3, step = 2, partialWindows = true)
+        .map {it.toFrame() }
         .map { it.value() }
         .sum()
 
-private fun List<String>.value(): Int = when {
-    this[1] == "/" -> 10 + this[2].toInt()
-    else -> this[0].toInt() + this[1].toInt()
+private fun List<String>.toFrame(): Frame = Frame(this[0], this[1], if (size > 2) this[2] else null)
+
+data class Frame(val first:String, val second: String, val next: String?)
+
+private fun Frame.value(): Int = when {
+    second == "/" -> 10 + (next?.toInt() ?: 0)
+    else -> first.toInt() + second.toInt()
 }
