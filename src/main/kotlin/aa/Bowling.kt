d246a8e788a96f2de5a8165de12cd71155a43910
Antoine Alberti <aa@antoinealberti.com>
2018-07-11 16:25:01 +0200

    replace List<Frame> with chained frames toFrames is now tail recursive

--- src/main/kotlin/aa/Bowling.kt
+++ src/main/kotlin/aa/Bowling.kt
@@ -1,37 +1,36 @@
 package aa
 
 fun score(vararg tries: String): Int = tries.toList()
-        .toFrames(Incomplete(42, 42))
-        .map { it.value() }
-        .sum()
+        .toFrames(null)
+        .score()
 
-private fun List<String>.toFrames(next: Frame): List<Frame> = when {
-    isEmpty() -> emptyList()
-    else -> {
-        val frameSize = if (last() == "X") 1 else 2
-        val frame = takeLast(frameSize).toFrame(next)
-        dropLast(frameSize).toFrames(frame) + frame
-    }
-}
+private fun List<String>.toFrames(next: Frame?): Frame =
+        if (isEmpty()) next!!
+        else {
+            val frameSize = if (last() == "X") 1 else 2
+            val frame = takeLast(frameSize).toFrame(next)
+            dropLast(frameSize).toFrames(frame)
+        }
 
-private fun List<String>.toFrame(next: Frame): Frame = when {
+private fun List<String>.toFrame(next: Frame?): Frame = when {
     this[0] == "X" -> Strike(next)
     this[1] == "/" -> Spare(this[0].toInt(), next)
-    else -> Incomplete(this[0].toInt(), this[1].toInt())
+    else -> Incomplete(this[0].toInt(), this[1].toInt(), next)
 }
 
-sealed class Frame(open val first: Int) {
+sealed class Frame(open val first: Int, open val next: Frame?) {
     abstract fun value(): Int
+    fun score(): Int = value() + (next?.score() ?: 0)
 }
 
-data class Strike(val next: Frame) : Frame(10) {
-    override fun value(): Int = 10 + next.value()
+data class Strike(override val next: Frame?) : Frame(10, next) {
+    override fun value(): Int = 10 + (next?.value() ?: 42)
 }
 
-data class Spare(override val first: Int, val next: Frame) : Frame(first) {
-    override fun value() = 10 + next.first
+data class Spare(override val first: Int, override val next: Frame?) : Frame(first, next) {
+    override fun value() = 10 + (next?.first ?: 42)
 }
 
-data class Incomplete(override val first: Int, val second: Int) : Frame(first) {
+data class Incomplete(override val first: Int, val second: Int, override val next: Frame?) : Frame(first, next) {
     override fun value() = this.first + this.second
 }
