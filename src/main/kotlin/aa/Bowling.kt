b6f157a7f44f01888f0b5e37023a9601a9e7af1b
Antoine Alberti <aa@antoinealberti.com>
2018-07-11 16:29:10 +0200

    restrain internal classes visibility

--- src/main/kotlin/aa/Bowling.kt
+++ src/main/kotlin/aa/Bowling.kt
@@ -18,19 +18,19 @@ private fun List<String>.toFrame(next: Frame?): Frame = when {
     else -> Incomplete(this[0].toInt(), this[1].toInt(), next)
 }
 
-sealed class Frame(open val first: Int, open val next: Frame?) {
+private sealed class Frame(open val first: Int, open val next: Frame?) {
     abstract fun value(): Int
     fun score(): Int = value() + (next?.score() ?: 0)
 }
 
-data class Strike(override val next: Frame?) : Frame(10, next) {
+private data class Strike(override val next: Frame?) : Frame(10, next) {
     override fun value(): Int = 10 + (next?.value() ?: 42)
 }
 
-data class Spare(override val first: Int, override val next: Frame?) : Frame(first, next) {
+private data class Spare(override val first: Int, override val next: Frame?) : Frame(first, next) {
     override fun value() = 10 + (next?.first ?: 42)
 }
 
-data class Incomplete(override val first: Int, val second: Int, override val next: Frame?) : Frame(first, next) {
+private data class Incomplete(override val first: Int, val second: Int, override val next: Frame?) : Frame(first, next) {
     override fun value() = this.first + this.second
 }
